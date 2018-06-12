12a09741a84f03450c63cf478e3590f95e400618
Phillip <phil.winston2@gmail.com>
2018-06-11 22:31:12 -0400

    base code

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+ before_action :authenticate_user!
+
+ def index
+   if current_user
+     @posts = current_user.posts.all
+   end
+ end
+
+ def new
+   @post = Post.new
+ end
+
+ def create
+   if current_user
+     @post = current_user.posts.new(post_params)
+   end
+   @post.save
+   redirect_to('/posts')
+ end
+
+ def show
+   @post = Post.find(params[:id])
+ end
+
+ def edit
+   @post = Post.find(params[:id])
+ end
+
+ def update
+   @post = Post.find(params[:id])
+   @post.update(post_params)
+   redirect_to('/posts')
+ end
+
+ def destroy
+   @post = Post.find(params[:id])
+   @post.destroy
+   redirect_to('/posts')
+ end
+
+
+ private
+ def post_params
+   return params[:post].permit(:content, :image)
+ end
+
+end
